14e45dd0f543c784afeef1c9a7729730c9f49491
SudoChips <Sbeberman37@gmail.com>
2025-05-25 13:09:21 -0400

    Add files via upload

---
+++ AreaOfShapes/AreaAndPerimeterOfACircle.java
@@ -0,0 +1,13 @@
+public class AreaAndPerimeterOfACircle{
+public static void main(String[] args){
+
+    double radius = 5.5;
+    double perimeter;
+    double area;
+
+    perimeter = 2 * radius * Math.PI;
+    area = radius * radius * Math.PI;
+
+    System.out.print("The perimeter is " + perimeter + "\nThe area is " + area + '\n');
+    }
+}
